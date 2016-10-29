b53bd265965eb5c15fedf908846c6065538be195
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-29 12:46:16 +0300

    Euclidean Gcd for two numbers added

--- Task1.Logic/GcdMaster.cs
+++ Task1.Logic/GcdMaster.cs
@@ -8,6 +8,31 @@ namespace Task1.Logic
 {
     public static class GcdMaster
     {
+        public static long? Gcd(long x, long y)
+        {
+            if (x == 0 && y == 0)
+                return null;
+            return ComputeGcd(x, y);
+        }
 
+        private static long ComputeGcd(long x, long y)
+        {
+            if (x == 0)
+                return y;
+            while (x != 0 && y != 0)
+            {
+                if (x < y)
+                {
+                    y %= x;
+                }
+                else
+                {
+                    x %= y;
+                }
+            }
+            if (x == 0)
+                return y;
+            return x;
+        }
     }
 }
