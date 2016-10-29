f7aac087fbb50b438e7fc3b3189ca8dc064d50b0
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-29 13:35:44 +0300

    Tests for Gcd with three parameters added

--- Task1.Logic.Tests/GcdMasterTests.cs
+++ Task1.Logic.Tests/GcdMasterTests.cs
@@ -39,9 +39,51 @@ namespace Task1.Logic.Tests
         [TestCase(36, -48, ExpectedResult = 12,
             Description = "First > 0, second < 0")]
         [Test]
-        public long? Gcd_Arg1Arg2_GcdReturns(long x, long y)
+        public long? Gcd_Arg1Arg2_GcdReturns(long a, long b)
         {
-            return GcdMaster.Gcd(x, y);
+            return GcdMaster.Gcd(a, b);
+        }
+
+        [TestCase(0, 0, 0, ExpectedResult = null,
+            Description = "Three zeroes haven't gcd")]
+        [TestCase(0, 0, 25, ExpectedResult = 25,
+            Description = "First and second number are zeroes")]
+        [TestCase(25, 0, 0, ExpectedResult = 25,
+            Description = "Second and third number are zeroes")]
+        [TestCase(0, 25, 0, ExpectedResult = 25,
+            Description = "First and third number are zeroes")]
+        [TestCase(10, 30, 60, ExpectedResult = 10,
+            Description = "First number is gcd")]
+        [TestCase(56, 28, 84, ExpectedResult = 28,
+            Description = "Second number is gcd")]
+        [TestCase(56, 84, 28, ExpectedResult = 28,
+            Description = "Third number is gcd")]
+        [TestCase(54, 48, 36, ExpectedResult = 6,
+            Description = "First nubmer > second > third.")]
+        [TestCase(36, 48, 54, ExpectedResult = 6,
+            Description = "First < second < third")]
+        [TestCase(36, 11, 22, ExpectedResult = 1,
+            Description = "second is minimum, gcd = 1")]
+        [TestCase(11, 36, 22, ExpectedResult = 1,
+            Description = "First is minimum, gcd = 1")]
+        [TestCase(-48, -16, -32, ExpectedResult = 16,
+            Description = "Three negative numbers, gcd = abs(second)")]
+        [TestCase(-16, -32, -48, ExpectedResult = 16,
+            Description = "Three negative nubmers, gcd = abs(first)")]
+        [TestCase(-32, -48, -16, ExpectedResult = 16,
+            Description = "Three negative nubmers, gcd = abs(third)")]
+        [TestCase(-48, -36, -24, ExpectedResult = 12,
+            Description = "Two negative numbers. abs(first) > abs(second) > abs(third)")]
+        [TestCase(-48, 36, 24, ExpectedResult = 12,
+            Description = "First < 0, second > 0, third > 0")]
+        [TestCase(36, -48, 24, ExpectedResult = 12,
+            Description = "First > 0, second < 0, third > 0")]
+        [TestCase(36, 48, -24, ExpectedResult = 12,
+            Description = "First > 0, second < 0, third < 0")]
+        [Test]
+        public long? Gcd_Arg1Arg2Arg3_GcdReturns(long a, long b, long c)
+        {
+            return GcdMaster.Gcd(a, b, c);
         }
     }
 }
--- Task1.Logic/GcdMaster.cs
+++ Task1.Logic/GcdMaster.cs
@@ -8,31 +8,47 @@ namespace Task1.Logic
 {
     public static class GcdMaster
     {
-        public static long? Gcd(long x, long y)
+        /// <summary>
+        /// Computes GCD of two numbers
+        /// </summary>
+        /// <returns>null if both numbers are zeroes, GCD if not</returns>
+        public static long? Gcd(long a, long b)
         {
-            if (x == 0 && y == 0)
+            if (a == 0 && b == 0)
                 return null;
-            return ComputeGcd(Math.Abs(x), Math.Abs(y));
+            return ComputeGcd(Math.Abs(a), Math.Abs(b));
         }
 
-        private static long ComputeGcd(long x, long y)
+        /// <summary>
+        /// Computes GCD of three numbers
+        /// </summary>
+        /// <returns> null if all numbers equal to zero, GCD if not</returns>
+        public static long? Gcd(long a, long b, long c)
         {
-            if (x == 0)
-                return y;
-            while (x != 0 && y != 0)
+            long ret = ComputeGcd(ComputeGcd(Math.Abs(a), Math.Abs(b)), Math.Abs(c));
+            if (ret == 0)
+                return null;
+            return ret;
+        }
+
+        private static long ComputeGcd(long a, long b)
+        {
+            if (a == 0)
+                return b;
+            while (a != 0 && b != 0)
             {
-                if (x < y)
+                if (a < b)
                 {
-                    y %= x;
+                    b %= a;
                 }
                 else
                 {
-                    x %= y;
+                    a %= b;
                 }
             }
-            if (x == 0)
-                return y;
-            return x;
+            if (a == 0)
+                return b;
+            return a;
         }
     }
 }
