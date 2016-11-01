300faf8f6c5f5b5d102037d1fc329933b5a9a3f5
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-01 23:06:18 +0300

    Documentation for RowComparators added

--- Task2.Logic/RowComparators.cs
+++ Task2.Logic/RowComparators.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace Task2.Logic
 {
+    /// <summary>
+    /// Compares two sz-arrays by row's sums in ascending order
+    /// </summary>
     public class RowSumComparatorAscending : IRowsComparator
     {
+        /// <summary>
+        /// Compares two sz-arrays by sum of elements
+        /// </summary>
+        /// <param name="row1">First array</param>
+        /// <param name="row2">Second array</param>
+        /// <returns>Value greater than 0 if sum of elements in
+        /// <paramref name="row1"/> is greater than sum of elements in
+        /// <paramref name="row2"/>. Zero if sums are equals. Less than
+        /// 0 if sum of elements in <paramref name="row1"/> is less than
+        /// sum of elements in <paramref name="row2"/></returns>
         public int Compare(long[] row1, long[] row2)
         {
             if (row1 == null)
@@ -21,17 +34,30 @@ namespace Task2.Logic
             if (row2.Length == 0)
                 throw new ArgumentException($"{nameof(row2)} has no elements");
             long s1 = 0;
-            for (int i = 0; i < row1.Length; i++)
-                s1 = checked(s1 + row1[i]);
+            foreach (long t in row1)
+                s1 = checked(s1 + t);
             long s2 = 0;
-            for (int i = 0; i < row2.Length; i++)
-                s2 = checked(s2 + row2[i]);
+            foreach (long t in row2)
+                s2 = checked(s2 + t);
             return s1.CompareTo(s2);
         }
     }
 
+    /// <summary>
+    /// Compares two sz-arrays by row's sums in ascending order
+    /// </summary>
     public class RowSumComparatorDescending : IRowsComparator
     {
+        /// <summary>
+        /// Compares two sz-arrays by sum of elements
+        /// </summary>
+        /// <param name="row1">First array</param>
+        /// <param name="row2">Second array</param>
+        /// <returns>Value greater than 0 if sum of elements in
+        /// <paramref name="row2"/> is greater than sum of elements in
+        /// <paramref name="row1"/>. Zero if sums are equals. Less than
+        /// 0 if sum of elements in <paramref name="row2"/> is less than
+        /// sum of elements in <paramref name="row1"/></returns>
         public int Compare(long[] row1, long[] row2)
         {
             if (row1 == null)
@@ -55,8 +81,21 @@ namespace Task2.Logic
         }
     }
 
+    /// <summary>
+    /// Compares two sz-arrays by row's maximums in ascending order
+    /// </summary>
     public class RowMaxComparatorAscending : IRowsComparator
     {
+        /// <summary>
+        /// Compares two sz-arrays by maximum element
+        /// </summary>
+        /// <param name="row1">First array</param>
+        /// <param name="row2">Second array</param>
+        /// <returns>Value greater than 0 if maximum element in
+        /// <paramref name="row1"/> is greater than maximum element in
+        /// <paramref name="row2"/>. Zero if sums are equals. Less than
+        /// 0 if maximum element in <paramref name="row1"/> is less than
+        /// maximum element in <paramref name="row2"/></returns>
         public int Compare(long[] row1, long[] row2)
         {
             if (row1 == null)
@@ -79,8 +118,21 @@ namespace Task2.Logic
         }
     }
 
+    /// <summary>
+    /// Compares two sz-arrays by row's maximums in descending order
+    /// </summary>
     public class RowMaxComparatorDescending : IRowsComparator
     {
+        /// <summary>
+        /// Compares two sz-arrays by maximum element
+        /// </summary>
+        /// <param name="row1">First array</param>
+        /// <param name="row2">Second array</param>
+        /// <returns>Value greater than 0 if maximum element in
+        /// <paramref name="row2"/> is greater than maximum element in
+        /// <paramref name="row1"/>. Zero if sums are equals. Less than
+        /// 0 if maximum element in <paramref name="row2"/> is less than
+        /// maximum element in <paramref name="row1"/></returns>
         public int Compare(long[] row1, long[] row2)
         {
             if (row1 == null)
@@ -103,8 +155,21 @@ namespace Task2.Logic
         }
     }
 
+    /// <summary>
+    /// Compares two sz-arrays by row's minimums in ascending order
+    /// </summary>
     public class RowMinComparatorAscending : IRowsComparator
     {
+        /// <summary>
+        /// Compares two sz-arrays by maximum element
+        /// </summary>
+        /// <param name="row1">First array</param>
+        /// <param name="row2">Second array</param>
+        /// <returns>Value greater than 0 if minimum element in
+        /// <paramref name="row1"/> is greater than minimum element in
+        /// <paramref name="row2"/>. Zero if sums are equals. Less than
+        /// 0 if minimum element in <paramref name="row1"/> is less than
+        /// minimum element in <paramref name="row2"/></returns>
         public int Compare(long[] row1, long[] row2)
         {
             if (row1 == null)
@@ -127,8 +192,21 @@ namespace Task2.Logic
         }
     }
 
+    /// <summary>
+    /// Compares two sz-arrays by row's minimums in descending order
+    /// </summary>
     public class RowMinComparatorDescending : IRowsComparator
     {
+        /// <summary>
+        /// Compares two sz-arrays by maximum element
+        /// </summary>
+        /// <param name="row1">First array</param>
+        /// <param name="row2">Second array</param>
+        /// <returns>Value greater than 0 if minimum element in
+        /// <paramref name="row2"/> is greater than minimum element in
+        /// <paramref name="row1"/>. Zero if sums are equals. Less than
+        /// 0 if minimum element in <paramref name="row2"/> is less than
+        /// minimum element in <paramref name="row1"/></returns>
         public int Compare(long[] row1, long[] row2)
         {
             if (row1 == null)
